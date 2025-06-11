dfd3c9d67841c8f2e56996fa42e9af25b7a1b7b9
xcodegou <xcodegou@tencent.com>
2025-06-11 16:14:10 +0800

    ZipArchive依赖版本修改
    Signed-off-by: xcodegou <xcodegou@tencent.com>

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
         .package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.9"),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.6.0"),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.4.3"),
         .package(url: "https://github.com/xcodegou/Brotli.git", branch: "main"),
         .package(url: "https://github.com/xcodegou/MQQComponents.git", branch: "main"),
         .package(url: "https://github.com/xcodegou/PromiseObjC.git", branch: "main"),
