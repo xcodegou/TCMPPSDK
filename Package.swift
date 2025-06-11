4733b4270fe6d4aa51367e50c68d4d4afe753147
xcodegou <xcodegou@tencent.com>
2025-06-11 17:48:20 +0800

    update depend
    Signed-off-by: xcodegou <xcodegou@tencent.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "TCMPPSDK",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
@@ -19,12 +20,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
         .package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.9"),
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.4.3"),
-        .package(url: "https://github.com/xcodegou/Brotli.git", branch: "main"),
+        .package(url: "https://github.com/xcodegou/SSZipArchive.git", branch: "master"),
+        .package(url: "https://github.com/xcodegou/Brotli.git", branch: "master"),
         .package(url: "https://github.com/xcodegou/MQQComponents.git", branch: "main"),
         .package(url: "https://github.com/xcodegou/PromiseObjC.git", branch: "main"),
         .package(url: "https://github.com/xcodegou/TMFShark.git", branch: "main"),
-        .package(url: "https://github.com/xcodegou/SocketRocket.git", branch: "main"),
+        .package(url: "https://github.com/xcodegou/SocketRocket.git", branch: "master"),
         .package(url: "https://github.com/xcodegou/Tars.git", branch: "main"),
     ],
     targets: [
@@ -38,7 +39,7 @@ let package = Package(
                 "TCMPPSDK",
                 .product(name: "CocoaAsyncSocket", package: "CocoaAsyncSocket"),
                 .product(name: "MJRefresh", package: "MJRefresh"),
-                .product(name: "ZipArchive", package: "ZipArchive"),
+                .product(name: "SSZipArchive", package: "SSZipArchive"),
                 .product(name: "Brotli", package: "Brotli"),
                 .product(name: "MQQComponents", package: "MQQComponents"),
                 .product(name: "PromiseObjC", package: "PromiseObjC"),
